7c5c16bcea77781735a724abe8320a262440b583
allenjprice <allenjprice@gmail.com>
2017-04-18 21:28:31 -0700

    add logger middleware

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const PORT = 1337;
 const IP = '127.0.0.1';
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(`SERVING: ${req.method} to ${req.url}`);
+  next();
+});
+
 // serve static assets
 app.use('/client', express.static(__dirname + '/client'));
 
 //serve index.html on GET /
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.sendFile(__dirname + '/client/index.html');
 });
 
