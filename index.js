e1ef40d56e88b18818a5dc43853daf8e4310e08a
allenjprice <allenjprice@gmail.com>
2017-04-18 21:10:47 -0700

    serve static assets

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const PORT = 1337;
 const IP = '127.0.0.1';
 const app = express();
 
+// serve static assets
+app.use('/client', express.static(__dirname + '/client'));
+
 //serve index.html on GET /
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client/index.html');
 });
 
-//
 
 app.listen(PORT, () => {
   console.log(`learn-you-hangukmal express edition is listening on port ${PORT}`);
