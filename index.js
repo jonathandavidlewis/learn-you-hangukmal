3a212ae614254aeda9ce2a8d0dd16051e5074ab2
allenjprice <allenjprice@gmail.com>
2017-04-18 21:34:17 -0700

    refactor logger to its own module

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require('express');
 
 const phrases = require('./data/phrases');
+const logger = require('./middleware/logger');
 
 const PORT = 1337;
 const IP = '127.0.0.1';
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(`SERVING: ${req.method} to ${req.url}`);
-  next();
-});
+app.use(logger);
 
 // serve static assets
 app.use('/client', express.static(__dirname + '/client'));
